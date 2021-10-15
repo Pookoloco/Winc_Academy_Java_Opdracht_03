8487058e86bb7cb73334836cff2d2091b2400dec
Pookoloco <90471681+Pookoloco@users.noreply.github.com>
2021-10-15 19:02:03 +0200

    Add files via upload

---
+++ Opdracht_03/script.js
@@ -0,0 +1,40 @@
+const age = 18; 
+
+  if (age == 18) {
+
+    console.log("Je mag naar binnen!");
+
+} else if(age > 18) {
+
+    console.log("Je mag naar binnen!");
+
+} else {
+
+    console.log("Je mag niet naar binnen")
+
+}
+
+const isFemale = false;
+
+  if (isFemale == true) {
+
+    console.log("Het is vandaag Ladiesnight!");
+    
+} else {
+
+    console.log("Veel plezier!")
+
+} 
+
+const driverStatus ='bob';
+
+if (driverStatus == 'bob') {
+
+    console.log("Je mag veilig de weg op");
+
+} else {
+
+    console.log("Je mag niet rijden dus bel een taxi!");
+    
+}
+
